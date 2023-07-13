fef79904afa431688921af8ce223ac7f03e1abe3
togachev <togachev@gmail.com>
2023-07-13 15:38:33 +0500

    Footer translation

--- nextgisweb/pyramid/nodepkg/footer/footer.js
+++ nextgisweb/pyramid/nodepkg/footer/footer.js
@@ -1,10 +1,11 @@
 import "./footer.less";
+import i18n from "@nextgisweb/pyramid/i18n";
 
 export default function footer() {
 
     return (
         <>
-            © {new Date().getFullYear()} АУ «Югорский НИИ информационных технологий»
+            © {new Date().getFullYear()} {i18n.gettext("Yugorsky research Institute of information technologies")}
         </>
     );
 }
