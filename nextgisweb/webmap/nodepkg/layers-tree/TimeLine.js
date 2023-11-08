57eeab0a1eaafdc8d1df0a5bc22ea31f6b7782ee
togachev <togachev@gmail.com>
2023-11-08 17:53:37 +0500

    test datepicker

--- nextgisweb/webmap/nodepkg/layers-tree/TimeLine.js
+++ nextgisweb/webmap/nodepkg/layers-tree/TimeLine.js
@@ -1,7 +1,14 @@
-import { Slider, Card } from "@nextgisweb/gui/antd";
+import { useEffect, useMemo, useState } from "react";
+import { Dropdown, Slider, DatePicker } from "@nextgisweb/gui/antd";
 import { HistoryOutlined } from '@ant-design/icons';
 import "./TimeLine.less";
 import ParseDesc from "./ParseDesc"
+import { gettext } from "@nextgisweb/pyramid/i18n";
+
+const msgShowTimeLime = gettext("Show time line");
+const msgHideTimeLime = gettext("Hide time line");
+
+const { RangePicker } = DatePicker;
 
 export function TimeLine({
     nodeData,
@@ -10,52 +17,61 @@ export function TimeLine({
 }) {
     const { id } = nodeData;
 
+    const [value, setValue] = useState({minDate: '', maxDate: ''});
+    
+    useEffect(() => {
+        console.log(value);
+    }, [value]);
+
     if (timeLineClickId === undefined || timeLineClickId !== id) {
         return (
-            <span title="Описание" className="more"
+            <span title={msgShowTimeLime} className="more"
                 onClick={(e) => { setTimeLineClickId(id); e.stopPropagation(); }} >
                 <HistoryOutlined />
             </span>
         );
     }
-    const menuProps = {
-        items: [{
-            key: nodeData.id,
-            label: (
-                <Card
-                    bordered={false}
-                    style={{
-                        width: 350,
-                    }}
-                >
-                    <ParseDesc item={nodeData} />
-                </Card>
-            )
-        }],
-    };
 
     const onOpenChange = () => {
         setTimeLineClickId(undefined);
     };
 
     return (
-        <div className="timeline-block">
-            <Slider
-                range={{
-                    draggableTrack: true
-                }}
-                min={Math.floor(new Date("2023.10.01").valueOf() / 1000)}
-                max={Math.floor(new Date("2023.10.31").valueOf() / 1000)}
-                defaultValue={[
-                    Math.floor(new Date("2023.10.02").valueOf() / 1000),
-                    Math.floor(new Date("2023.10.16").valueOf() / 1000)
-                ]}
-                onChange={(item) => {
-                    let dateMin = new Date(item[0] * 1000).toLocaleDateString('en-US');
-                    let dateMax = new Date(item[1] * 1000).toLocaleDateString('en-US');
-                    console.log(dateMin, dateMax);
-                }}
-            />
-        </div>
+        <Dropdown
+            onOpenChange={onOpenChange}
+            trigger={["click"]}
+            open
+            dropdownRender={() => (
+                <span className="timeline-content" onClick={(e) => { e.stopPropagation(); }}>
+                    <Slider
+                        range={{
+                            draggableTrack: true
+                        }}
+                        min={Math.floor(new Date("2023.10.01").valueOf() / 1000)}
+                        max={Math.floor(new Date("2023.10.31").valueOf() / 1000)}
+                        defaultValue={[
+                            Math.floor(new Date("2023.10.02").valueOf() / 1000),
+                            Math.floor(new Date("2023.10.16").valueOf() / 1000)
+                        ]}
+                        onChange={(item) => {
+                            setValue({minDate: item[0], maxDate: item[1]});
+                        }}
+                    />
+                    <RangePicker className="range-picker"
+                    onChange={
+                        (item) => {
+                            let d0 = new Date(item[0].$D, item[0].$M, item[0].$y);
+                            let d1 = new Date(item[1].$D, item[1].$M, item[1].$y);
+                            setValue({minDate: d0, maxDate: item[1]})
+                        }
+                    }
+                    />
+                </span>
+            )} >
+            <span title={msgHideTimeLime} className="more"
+                onClick={(e) => { setTimeLineClickId(id); e.stopPropagation(); }} >
+                <HistoryOutlined />
+            </span>
+        </Dropdown>
     );
 }
