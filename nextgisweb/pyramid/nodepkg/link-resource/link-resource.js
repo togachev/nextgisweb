08d2eab9f63afa1810d432aaed94d4ed6b9c25cf
togachev <togachev@gmail.com>
2023-12-09 11:55:33 +0500

    fixing code for creating a link from a map to resources

--- nextgisweb/pyramid/nodepkg/link-resource/link-resource.js
+++ nextgisweb/pyramid/nodepkg/link-resource/link-resource.js
@@ -1,30 +1,14 @@
 import { routeURL } from "@nextgisweb/pyramid/api";
-import React from 'react';
-import { useState, useEffect } from "react";
 import { gettext } from "@nextgisweb/pyramid/i18n";
 import ResourceGroup from "./icons/resource_group.svg";
 
 import "./link-resource.less";
 
 export function linkResource() {
-
-	const [link, setLink] = useState([]);
-
-	const links_show = async () => {
-		const url = routeURL("resource.show", 0);
-		return url;
-
-	}
-
-	useEffect(() => {
-		links_show().then(url =>
-			setLink(<a title={gettext("Resources")} className="link-resource" href={url}>
-				<ResourceGroup />
-			</a>)
-		)
-	}, [])
-
+	const url = routeURL("resource.show", 0);
 	return (
-		<>{link}</>
+		<a title={gettext("Resources")} className="link-resource" href={url}>
+			<ResourceGroup />
+		</a>
 	);
 }
