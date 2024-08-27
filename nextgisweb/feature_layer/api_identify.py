ae1d8651b10ba29121f685407d217b22ef959407
togachev <togachev@gmail.com>
2024-08-27 17:53:46 +0500

    test identify module 2

--- nextgisweb/feature_layer/api_identify.py
+++ nextgisweb/feature_layer/api_identify.py
@@ -5,7 +5,6 @@ from nextgisweb.pyramid import JSONType
 from nextgisweb.resource import DataScope, Resource, ResourceScope
 
 from .interface import IFeatureLayer
-from .api import serialize
 
 def identify(request) -> JSONType:
     data = request.json_body
@@ -67,44 +66,6 @@ def identify(request) -> JSONType:
             feature_count += len(features)
 
     result["featureCount"] = feature_count
-
-    return result
-
-def query_feature_intersects(query, geom):
-    if geom is not None:
-        query.intersects(geom)
-        for feat in query():
-            return feat
-
-def identify_layer(request) -> JSONType:
-    data = request.json_body
-    srs = int(data["srs"])
-    geom = Geometry.from_wkt(data["geom"], srid=srs)
-    styles = map(int, data["styles"])
-    result = []
-    style_list = DBSession.query(Resource).filter(Resource.id.in_(styles))
-    
-    for style in style_list:
-        layer = style.parent
-        srs = request.GET.get("srs")
-
-        srlz_params = dict(
-            geom_format=request.GET.get("geom_format", "wkt").lower(),
-        )
-
-        query = layer.feature_query()
-        query.geom()
-
-        feature = query_feature_intersects(query, geom)
-
-        if feature is not None:
-            items = dict(
-                serialize(feature, **srlz_params),
-                column_from_const=layer.column_from_const,
-                column_key=layer.column_key,
-                column_constraint=layer.column_constraint
-            )
-            result.append(items)
 
     return result
 
@@ -209,12 +170,6 @@ def setup_pyramid(comp, config):
         "feature_layer.identify",
         "/api/feature_layer/identify",
         post=identify,
-    )
-
-    config.add_route(
-        "feature_layer.identify_layer",
-        "/api/feature_layer/identify_layer",
-        post=identify_layer,
     )
 
     config.add_route(
