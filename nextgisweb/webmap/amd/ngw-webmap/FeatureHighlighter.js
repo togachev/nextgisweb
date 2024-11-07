4ceb86fdb3d03a60d76117ad59c5c6799e1d8c75
togachev <togachev@gmail.com>
2024-11-07 12:54:17 +0500

    style FeatureHighlighter

--- nextgisweb/webmap/amd/ngw-webmap/FeatureHighlighter.js
+++ nextgisweb/webmap/amd/ngw-webmap/FeatureHighlighter.js
@@ -7,7 +7,8 @@ define([
     "@nextgisweb/pyramid/api",
     "ngw-webmap/ol/layer/Vector",
     "openlayers/ol",
-], function (declare, lang, xhr, topic, Deferred, api, Vector, ol) {
+    "@nextgisweb/pyramid/settings!webmap",
+], function (declare, lang, xhr, topic, Deferred, api, Vector, ol, settings) {
     return declare("ngw-webmap.FeatureHighlighter", [], {
         _map: null,
         _source: null,
@@ -33,14 +34,6 @@ define([
         },
 
         _getDefaultStyle: function () {
-            const pointClick =
-                `<svg id="icon-point-click" version="1.1" viewBox="0 0 13 13" width="13" height="13" xmlns="http://www.w3.org/2000/svg">
-                    <g>
-                        <path d="m6.5 0c-3.58 0-6.5 2.92-6.5 6.5s2.92 6.5 6.5 6.5 6.5-2.92 6.5-6.5-2.92-6.5-6.5-6.5z" fill="#106a92" stroke-width=".929"/>
-                        <path d="m5.99 11.5c-2.41-0.248-4.3-2.15-4.54-4.57-0.259-2.58 1.56-4.99 4.12-5.46 2.56-0.472 5.08 1.08 5.8 3.57 0.545 1.89-0.0295 3.89-1.5 5.2-1.05 0.943-2.49 1.41-3.88 1.27z" fill="#fff" stroke-width=".0242"/>
-                        <path d="m6.23 9.21c-1.29-0.133-2.31-1.16-2.44-2.46-0.139-1.39 0.839-2.68 2.21-2.94 1.38-0.254 2.73 0.582 3.12 1.92 0.293 1.02-0.0158 2.09-0.804 2.8-0.567 0.507-1.34 0.758-2.09 0.681z" stroke-width=".013"/>
-                    </g>
-                </svg>`
             return new ol.style.Style({
                 stroke: new ol.style.Stroke({
                     width: 2.26,
@@ -49,11 +42,16 @@ define([
                 fill: new ol.style.Fill({
                     color: "rgba(255,255,255,0.5)",
                 }),
-                image: new ol.style.Icon({
-                    opacity: 1,
-                    src: "data:image/svg+xml," + encodeURIComponent(pointClick),
-                    scale: 1,
-                })
+                image: new ol.style.Circle({
+                    stroke: new ol.style.Stroke({
+                        width: 2.26,
+                        color: "rgba(255,255,0,1)",
+                    }),
+                    radius: settings.identify_radius,
+                    fill: new ol.style.Fill({
+                        color: "rgba(255,255,255,0.5)",
+                    }),
+                }),
             });
         },
 
