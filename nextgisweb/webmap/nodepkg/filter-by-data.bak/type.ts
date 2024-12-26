1f2067d634464e1d64be0aab8ec3bd53dda53915
togachev <togachev@gmail.com>
2024-12-26 17:53:26 +0500

    remove filter by data

--- nextgisweb/webmap/nodepkg/filter-by-data.bak/type.ts
+++
@@ -1,7 +0,0 @@
-import type { NgwAttributeType } from "@nextgisweb/feature-layer/type/type/FeatureItem";
-
-/** This attributes for loading to NGW */
-export type NgwAttributeValue = Record<string, NgwAttributeType>;
-
-/** This attributes for using in web */
-export type AppAttributes = Record<string, unknown>;
