eda190e9277bf37fa69a3eb5a792fd640f32f18d
togachev <togachev@gmail.com>
2025-01-28 22:19:48 +0500

    test image view

--- nextgisweb/resource/nodepkg/description/ImageView.tsx
+++ nextgisweb/resource/nodepkg/description/ImageView.tsx
@@ -3,13 +3,10 @@ import { Button } from "@nextgisweb/gui/antd";
 import { CloseIcon } from "@nextgisweb/gui/icon";
 import showModal from "@nextgisweb/gui/showModal";
 import { useControls } from "./controls/useControls";
-import { useState } from 'react';
-import { List, arrayMove } from 'react-movable';
 import "./ImageView.less";
 
 const ImagePortal = ({ attribs }) => {
     const { refs } = useControls();
-    const [items, setItems] = useState([<img key="img" src={attribs.src} alt="" />]);
 
     return (
         createPortal(
@@ -53,33 +50,7 @@ const ImagePortal = ({ attribs }) => {
                     />
                 </div>
                 <div className="image-block">
-                    <List
-                        values={items}
-                        onChange={({ oldIndex, newIndex }) =>
-                            setItems(arrayMove(items, oldIndex, newIndex))
-                        }
-                        renderList={({ children, props, isDragged }) => (
-                            <div
-                                {...props}
-                                style={{ padding: 0, cursor: isDragged ? 'grabbing' : undefined }}
-                            >
-                                {children}
-                            </div>
-                        )}
-                        renderItem={({ value, props, isDragged }) => (
-                            <div
-                                {...props}
-                                key={props.key}
-                                style={{
-                                    ...props.style,
-                                    cursor: isDragged ? 'grabbing' : 'grab',
-                                    zIndex: 1081,
-                                }}
-                            >
-                                {value}
-                            </div>
-                        )}
-                    />
+                    <img key="img" src={attribs.src} alt="" />
                 </div>
             </div >,
             document.body
