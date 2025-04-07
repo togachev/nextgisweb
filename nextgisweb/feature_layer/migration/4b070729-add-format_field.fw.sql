e4e3484d849b352b9165bddd45025784b4dc3f22
togachev <togachev@gmail.com>
2025-04-07 12:05:35 +0500

    format fields migration

---
+++ nextgisweb/feature_layer/migration/4b070729-add-format_field.fw.sql
@@ -0,0 +1,7 @@
+/*** {
+    "revision": "4b070729", "parents": ["42550420"],
+    "date": "2025-04-04T16:24:08",
+    "message": "Add format_field"
+} ***/
+
+ALTER TABLE layer_field ADD COLUMN format_field jsonb;
---
+++ nextgisweb/feature_layer/migration/4b070729-add-format_field.rw.sql
@@ -0,0 +1,3 @@
+/*** { "revision": "4b070729" } ***/
+
+ALTER TABLE layer_field DROP COLUMN format_field;
