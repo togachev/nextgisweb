392d7ee07ce3a6ee4f24d7bffb30218a957b59ac
togachev <togachev@gmail.com>
2025-04-14 15:53:36 +0500

    button style navigation menu bottom infomap

--- nextgisweb/webmap/nodepkg/display/component/NavigationMenu.tsx
+++ nextgisweb/webmap/nodepkg/display/component/NavigationMenu.tsx
@@ -2,7 +2,7 @@ import classNames from "classnames";
 import { observer } from "mobx-react-lite";
 import { useCallback } from "react";
 import { gettext } from "@nextgisweb/pyramid/i18n";
-import { Typography } from "@nextgisweb/gui/antd";
+import { Row, Col, Typography } from "@nextgisweb/gui/antd";
 
 import type { PanelManager } from "../../panel/PanelManager";
 import type { Display } from "../Display";
@@ -30,27 +30,31 @@ export const NavigationMenu = observer(({ store, display }: { store: PanelManage
 
     const active = store.activePanel;
     return (
-        <div className="ngw-webmap-display-navigation-menu">
-            {store.sorted().map(({ name, title, plugin }) => (
-                <div
-                    key={name}
-                    title={title}
-                    onClick={() => onClickItem(name)}
-                    className={classNames(
-                        "ngw-webmap-display-navigation-menu-item",
-                        { "active": name === active?.name }
-                    )}
-                >
-                    {plugin.icon}
-                </div>
-            ))}
-            {!display.tinyConfig && infomap.scope ? (
-                <div className="infoblock">
-                    <Link target="_blank" href={infomap.link}><span title={gettext("Map properties")} className="iconLinks"><Information /></span></Link>
-                    <Link target="_blank" href={infomap.update}><span title={gettext("Map settings")} className="iconLinks"><Cogs /></span></Link>
-                </div>
-            ) : null}
-        </div>
+        <Row className="ngw-webmap-display-navigation-menu">
+            <Col>
+                {store.sorted().map(({ name, title, plugin }) => (
+                    <div
+                        key={name}
+                        title={title}
+                        onClick={() => onClickItem(name)}
+                        className={classNames(
+                            "ngw-webmap-display-navigation-menu-item",
+                            { "active": name === active?.name }
+                        )}
+                    >
+                        {plugin.icon}
+                    </div>
+                ))}
+            </Col>
+            <Col>
+                {!display.tinyConfig && infomap.scope ? (
+                    <div className="infoblock">
+                        <Link target="_blank" href={infomap.link}><span title={gettext("Map properties")} className="iconLinks"><Information /></span></Link>
+                        <Link target="_blank" href={infomap.update}><span title={gettext("Map settings")} className="iconLinks"><Cogs /></span></Link>
+                    </div>
+                ) : null}
+            </Col>
+        </Row>
     );
 });
 
