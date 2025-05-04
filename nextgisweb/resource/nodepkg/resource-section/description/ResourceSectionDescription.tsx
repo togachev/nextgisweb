cce09df6d48d70fefd33428e3301de08c491eace
togachev <togachev@gmail.com>
2025-05-05 00:25:15 +0500

    ResourceSectionDescription setting collapse component

--- nextgisweb/resource/nodepkg/resource-section/description/ResourceSectionDescription.tsx
+++ nextgisweb/resource/nodepkg/resource-section/description/ResourceSectionDescription.tsx
@@ -16,8 +16,10 @@ export const ResourceSectionDescription: ResourceSection = ({
     const [descValue, setDescValue] = useState(undefined);
     const [spinning, setSpinning] = useState(false);
     const [percent, setPercent] = useState(0);
+    const [collapse, setCollapse] = useState(false);
 
     const showLoader = async () => {
+        setCollapse(true)
         setSpinning(true);
         let ptg = -10;
         const value = await route("resource.item", resourceData.resource.id).get({
@@ -47,17 +49,21 @@ export const ResourceSectionDescription: ResourceSection = ({
     const items: CollapseProps["items"] = [
         {
             key: "description",
-            label: !descValue ? gettext("View resource description") : gettext("Collapse description"),
+            label: descValue && collapse ? gettext("Collapse description") : gettext("View resource description"),
             children: descValue && <DescComponent content={descValue} />,
             extra: <Spin indicator={<LoadingOutlined spin />} spinning={spinning} percent={percent} />,
         },
     ];
 
+    const onChange = () => {
+        setCollapse(!collapse);
+    }
+
     return (
         <div className="description-panel">
             {!descValue ?
                 <Collapse size="large" items={items} onChange={showLoader} /> :
-                <Collapse size="large" items={items} defaultActiveKey={['description']} />
+                <Collapse size="large" items={items} defaultActiveKey={["description"]} onChange={onChange} />
             }
         </div>
     );
