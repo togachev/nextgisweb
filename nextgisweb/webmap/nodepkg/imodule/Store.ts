920c95e1600e06169b9086f86bc40bb308937a2f
togachev <togachev@gmail.com>
2025-05-22 08:10:14 +0500

    imodule popup share test

--- nextgisweb/webmap/nodepkg/imodule/Store.ts
+++ nextgisweb/webmap/nodepkg/imodule/Store.ts
@@ -223,11 +223,11 @@ export class Store {
                 const result = [...new Set(st?.map(a => a.styleId))];
 
                 const panel = this.display.panelManager.getActivePanelName();
-
+                const pn_ = pn !== "share_feature" ? pn : "attributes"
                 const obj = disable ?
-                    { attribute: false, lon, lat, zoom, styles: styles, st: result, slf: selected, pn: pn, base: this.display.map.baseLayer?.name } :
+                    { attribute: false, lon, lat, zoom, styles: styles, st: result, slf: selected, pn: pn_, base: this.display.map.baseLayer?.name } :
                     res ?
-                        { attribute: true, lon, lat, zoom, styles: styles, st: result, slf: selected, pn: pn, base: this.display.map.baseLayer?.name } :
+                        { attribute: true, lon, lat, zoom, styles: styles, st: result, slf: selected, pn: pn_, base: this.display.map.baseLayer?.name } :
                         { attribute: false, lon, lat, zoom, styles: styles, base: this.display.map.baseLayer?.name };
 
                 panel !== "share" && Object.assign(obj, { panel: panel });
