60bca9e4d1d679c3556d5b63d3b5a0478d0bca84
togachev <togachev@gmail.com>
2025-08-15 17:19:50 +0500

    mobile detect use code react-device-detect...

--- nextgisweb/webmap/nodepkg/mobile/types.ts
+++ nextgisweb/webmap/nodepkg/mobile/types.ts
@@ -2,6 +2,6 @@ import { DeviceTypes } from "./constant";
 import { isIOS13Check } from "./utils";
 
 export const isMobileAndTabletType = ({ type }) =>
-  type === DeviceTypes.Mobile || type === DeviceTypes.Tablet;
+	type === DeviceTypes.Mobile || type === DeviceTypes.Tablet;
 
 export const getIPad13 = () => isIOS13Check("iPad");
--- nextgisweb/webmap/nodepkg/mobile/utils.ts
+++ nextgisweb/webmap/nodepkg/mobile/utils.ts
@@ -1,19 +1,19 @@
 export const getNavigatorInstance = () => {
-  if (typeof window !== 'undefined') {
-    if (window.navigator || navigator) {
-      return window.navigator || navigator;
-    }
-  }
+	if (typeof window !== 'undefined') {
+		if (window.navigator || navigator) {
+			return window.navigator || navigator;
+		}
+	}
 
-  return false;
+	return false;
 };
 
 export const isIOS13Check = (type) => {
-  const nav = getNavigatorInstance();
-  return (
-    nav &&
-    nav.platform &&
-    (nav.platform.indexOf(type) !== -1 ||
-      (nav.platform === 'MacIntel' && nav.maxTouchPoints > 1 && !window.MSStream))
-  );
+	const nav = getNavigatorInstance();
+	return (
+		nav &&
+		nav.platform &&
+		(nav.platform.indexOf(type) !== -1 ||
+			(nav.platform === 'MacIntel' && nav.maxTouchPoints > 1 && !window.MSStream))
+	);
 };
