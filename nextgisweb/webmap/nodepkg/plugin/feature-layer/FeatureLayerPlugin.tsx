f94b7bd4052296865d6703a48ca1ba2e802371f7
togachev <togachev@gmail.com>
2025-08-27 12:35:20 +0500

    open table in new window

--- nextgisweb/webmap/nodepkg/plugin/feature-layer/FeatureLayerPlugin.tsx
+++ nextgisweb/webmap/nodepkg/plugin/feature-layer/FeatureLayerPlugin.tsx
@@ -1,12 +1,16 @@
 import { gettext } from "@nextgisweb/pyramid/i18n";
+import { routeURL } from "@nextgisweb/pyramid/api";
 import topic from "@nextgisweb/webmap/compat/topic";
 
 import { PluginBase } from "../PluginBase";
 
 import TableIcon from "@nextgisweb/icon/material/table";
+import OpenInNew from "@nextgisweb/icon/material/open_in_new";
+
+import type { LayerItemConfig } from "@nextgisweb/webmap/type/api";
 
 export class FeatureLayerPlugin extends PluginBase {
-    getMenuItem() {
+    getMenuItem(nodeData: LayerItemConfig) {
         return {
             icon: <TableIcon />,
             title: gettext("Feature table"),
@@ -14,6 +18,16 @@ export class FeatureLayerPlugin extends PluginBase {
                 this.openFeatureGrid();
                 return Promise.resolve(undefined);
             },
+            extra:
+                <span
+                    className="show-resource"
+                    title={gettext("Layer show")}
+                    onClick={() => {
+                        window.open(routeURL("feature_layer.feature.browse", { id: nodeData.layerId }), "_blank");
+                    }}
+                >
+                    <OpenInNew />
+                </span >,
         };
     }
 
